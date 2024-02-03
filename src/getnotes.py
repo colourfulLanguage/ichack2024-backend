775f7f9f040255b91dc4057d9d0467a2c465db4b
Trinity Stenhouse <trinity.stenhouse@btinternet.com>
2024-02-03 19:08:31 +0000

    now we have notes data in txt

--- src/getnotes.py
+++ src/getnotes.py
@@ -4,66 +4,18 @@ from io import BytesIO
 from pydub.playback import play
 from bs4 import BeautifulSoup
 
-file_path = 'src/piano_chromatic_scale.mp3'
-
-try:
-    with open(file_path, 'rb') as file:
-        audio_bytes = file.read()
-
-except Exception as e:
-    print(f"Error: {e}")
-
 def cut_audio(audio, start_ms, end_ms):
+    # returns actual sound
     audio_segment = AudioSegment.from_file(BytesIO(audio), format="mp3")
     cut_segment = audio_segment[start_ms:end_ms]
     return cut_segment
 
 def get_bytes_data(note):
+    # returns bytes data
     cut_bytesio = BytesIO()
     note.export(cut_bytesio, format="mp3")
     note_bytes = cut_bytesio.getvalue()  # get bytes data
     return note_bytes
-
-
-
-notes = ['C', 'Csharp', 'D', 'Dsharp', 'E', 'F', 'Fsharp', 'G', 'Gsharp', 'A', 'Asharp', 'B', 'C_high', 'Csharp_high']
-notes_dict = dict()
-for i in range(0, 7):
-    print(i)
-    note = cut_audio(audio_bytes, 1000*i, 1000*(i+1))
-    #play(note)
-    notes_dict[f"{notes[i]}"] = get_bytes_data(note)
-
-print(notes_dict)
-
-note = cut_audio(audio_bytes, 1000*7, 950*(7+1))
-#play(note)
-notes_dict[f"{notes[7]}"] = get_bytes_data(note)
-
-note = cut_audio(audio_bytes, 1200*7, 1200*8)
-#play(note)
-notes_dict[f"{notes[8]}"] = get_bytes_data(note)
-note = cut_audio(audio_bytes, 1200*8, 1300*8)
-#play(note)
-notes_dict[f"{notes[9]}"] = get_bytes_data(note)
-
-note = cut_audio(audio_bytes, 1300*8, 1250*9)
-#play(note)
-notes_dict[f"{notes[10]}"] = get_bytes_data(note)
-
-note = cut_audio(audio_bytes, 1250*9, 1250*10)
-#play(note)
-notes_dict[f"{notes[11]}"] = get_bytes_data(note)
-
-note = cut_audio(audio_bytes, 1250*10, 1200*11)
-#play(note)
-notes_dict[f"{notes[12]}"] = get_bytes_data(note)
-
-note = cut_audio(audio_bytes, 1200*11, 1150*12)
-#play(note)
-notes_dict[f"{notes[13]}"] = get_bytes_data(note)
-
-print(notes_dict.keys())
 
 def save_dict_to_txt(dictionary, file_path):
     try:
@@ -74,5 +26,65 @@ def save_dict_to_txt(dictionary, file_path):
 
     except Exception as e:
         print(f"Error: {e}")
+def generate_notes_dict(audio_bytes, testing=False):
+    notes = ['C', 'Csharp', 'D', 'Dsharp', 'E', 'F', 'Fsharp', 'G', 'Gsharp', 'A', 'Asharp', 'B', 'C_high', 'Csharp_high']
+    notes_dict = dict()
+    for i in range(0, 7):
+        note = cut_audio(audio_bytes, 1000*i, 1000*(i+1))
+        if testing:
+            play(note)
+        notes_dict[f"{notes[i]}"] = get_bytes_data(note)
 
-save_dict_to_txt(notes_dict, 'src/notes_data.txt')
+    note = cut_audio(audio_bytes, 1000*7, 950*(7+1))
+    if testing:
+        play(note)
+    notes_dict[f"{notes[7]}"] = get_bytes_data(note)
+
+    note = cut_audio(audio_bytes, 1200*7, 1200*8)
+    if testing:
+        play(note)
+    notes_dict[f"{notes[8]}"] = get_bytes_data(note)
+    note = cut_audio(audio_bytes, 1200*8, 1300*8)
+    if testing:
+        play(note)
+    notes_dict[f"{notes[9]}"] = get_bytes_data(note)
+
+    note = cut_audio(audio_bytes, 1300*8, 1250*9)
+    if testing:
+        play(note)
+    notes_dict[f"{notes[10]}"] = get_bytes_data(note)
+
+    note = cut_audio(audio_bytes, 1250*9, 1250*10)
+    if testing:
+        play(note)
+    notes_dict[f"{notes[11]}"] = get_bytes_data(note)
+
+    note = cut_audio(audio_bytes, 1250*10, 1200*11)
+    if testing:
+        play(note)
+    notes_dict[f"{notes[12]}"] = get_bytes_data(note)
+
+    note = cut_audio(audio_bytes, 1200*11, 1150*12)
+    if testing:
+        play(note)
+    notes_dict[f"{notes[13]}"] = get_bytes_data(note)
+
+    save_dict_to_txt(notes_dict, 'src/notes_data.txt')
+
+    return notes_dict
+
+if __name__ == "__main__":
+
+    file_path = 'src/piano_chromatic_scale.mp3'
+
+    try:
+        with open(file_path, 'rb') as file:
+            audio_bytes = file.read()
+
+    except Exception as e:
+        print(f"Error: {e}")
+
+    generate_notes_dict(audio_bytes)
+
+
+
